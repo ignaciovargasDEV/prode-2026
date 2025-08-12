173bbcc10d5277815302714e7124fd0735a64a58
Ignacio Vargas <vargasignaciomat@gmail.com>
2025-08-12 18:05:10 -0300

    build problems

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'standalone',
+  outputFileTracingIncludes: {
+    '/': ['./contexts/**/*'],
+  },
 }
 
 export default nextConfig
